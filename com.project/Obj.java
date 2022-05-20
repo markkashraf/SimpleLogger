e5fccde00d4a497bcfd1cb1d995dda6140582b66
mark <54954479+markkashraf@users.noreply.github.com>
2022-05-20 16:04:26 +0200

    Update Obj.java

--- com.project/Obj.java
+++ com.project/Obj.java
@@ -2,16 +2,17 @@ package com.project;
 
 import java.util.ArrayList;
 
-public class Object
+public class Obj
 {
     ArrayList<Field> Fields;
     String ObjectName;
+    ArrayList<Obj> InternalObjects;
     String Parent;
 
-    Object()
+    Obj()
     {
         Fields = new ArrayList<Field>();
-
+        ArrayList<Obj> InternalObjects = new ArrayList<Obj>();
     }
 
     public void AddFields(Field x)
