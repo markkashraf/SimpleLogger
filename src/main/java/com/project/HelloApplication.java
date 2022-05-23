c892c94ac6ef7e7b612429afe1d8b0a10f68ed0e
marcosherif <68437948+marcosherif@users.noreply.github.com>
2022-05-23 22:17:06 +0200

    gui first try

--- src/main/java/com/project/HelloApplication.java
+++ src/main/java/com/project/HelloApplication.java
@@ -1,14 +1,63 @@
 package com.project;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 
-public class HelloApplication {
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+
 
     public static void main(String[] args) throws Exception
     {
 
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
         XSSFSheet sheet = readexcel.getSheet1("src\\Test Excel File\\Example.xlsx");
 
         Service x = new Service(sheet);
+
+        primaryStage.setTitle("Main Window");
+
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(200);
+        grid.setHgap(200);
+
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+
+
+        //getItems of checkbox
+        choiceBox.getItems().add("Choose Operation");
+        for(int i =0; i < x.Operations.size(); i++) {
+            choiceBox.getItems().add(x.);
+
+        }
+
+        choiceBox.setValue("Choose Operation");
+
+
+        GridPane.setConstraints(choiceBox, 0, 0);
+
+
+        grid.getChildren().addAll(choiceBox);
+
+        Scene scene = new Scene(grid, 800, 400);
+
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
 }
+
