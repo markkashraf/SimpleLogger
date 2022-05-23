13eb1ab3854fb6ff1884eb87af83b01a6f24a11e
marcosherif <68437948+marcosherif@users.noreply.github.com>
2022-05-24 01:23:43 +0200

    Update HelloApplication.java

--- src/main/java/com/project/HelloApplication.java
+++ src/main/java/com/project/HelloApplication.java
@@ -3,7 +3,6 @@ package com.project;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
