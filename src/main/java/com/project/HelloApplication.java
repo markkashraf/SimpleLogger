ad16016861f8affdcdb6912713ace5f8e61b39d1
marcosherif <68437948+marcosherif@users.noreply.github.com>
2022-05-26 23:41:15 +0200

    Response done

--- src/main/java/com/project/HelloApplication.java
+++ src/main/java/com/project/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -27,12 +28,19 @@ public class HelloApplication extends Application
 
         Service x = new Service(sheet);
 
+        primaryStage.setTitle("HTTP Program");
+
+        //make scene scrollable
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setPannable(true);
+
+
         //declare grid
         GridPane grid = new GridPane();
         grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setVgap(15);
+        grid.setVgap(40);
         grid.setHgap(40);
-        grid.setGridLinesVisible(false);
+        grid.setGridLinesVisible(true);
 
 
         //declare choiceBox
@@ -117,12 +125,79 @@ public class HelloApplication extends Application
                 }
             }
 
+            rowIndex+= x.getOperation(newValue.intValue() - 1).getObjectFromRequest(x.getOperation(newValue.intValue() - 1).getRequestSize() - 1).getNumberofFields();
+
+            int offset = rowIndex;
+
+            //print "Response"
+            Text response = new Text("Response:");
+            response.setFont(Font.font(20));
+            GridPane.setConstraints(response, 0, offset);
+            grid.getChildren().addAll(response);
+
+            int rowIndex2 = offset;
+
+            //iterate over all objects in response in current operation
+            for(int i = 0; i < x.getOperation(newValue.intValue() - 1).getResponseSize(); i++){
+
+
+
+                Text object = new Text(x.getOperation(newValue.intValue() - 1).getObjectFromResponse(i).getObjectName() + ":");
+                object.setFont(Font.font(15));
+                if(i == 0)
+                {
+                    GridPane.setConstraints(object, 1, i + offset);
+                    System.out.println(x.getOperation(newValue.intValue() - 1).getObjectFromResponse(i).getNumberofFields());
+                }
+                else
+                {
+                    rowIndex2 += x.getOperation(newValue.intValue() - 1).getObjectFromResponse(i - 1).getNumberofFields();
+                    GridPane.setConstraints(object, 1, rowIndex2);
+                }
+
+                grid.getChildren().addAll(object);
+
+
+
+                //iterate over every field in current object
+                for(int j = 0; j < x.getOperation(newValue.intValue() - 1).getObjectFromResponse(i).getNumberofFields(); j++){
+
+                    Text field = new Text(x.getOperation(newValue.intValue()- 1).getObjectFromResponse(i).getField(j).getName());
+                    Text allowedValues = new Text(x.getOperation(newValue.intValue()- 1).getObjectFromResponse(i).getField(j).getAllowedValues());
+                    Text mandatory = new Text(x.getOperation(newValue.intValue()- 1).getObjectFromResponse(i).getField(j).getMandatory());
+
+
+                    field.setFont(Font.font(15));
+                    allowedValues.setFont(Font.font(15));
+                    mandatory.setFont(Font.font(15));
+                    if(i == 0)
+                    {
+                        GridPane.setConstraints(field, 2, j + offset);
+                        GridPane.setConstraints(allowedValues, 3, j + offset);
+                        GridPane.setConstraints(mandatory, 4, j + offset);
+
+                    }
+                    else
+                    {
+                        GridPane.setConstraints(field, 2, j + rowIndex2);
+                        GridPane.setConstraints(allowedValues, 3, j + rowIndex2);
+                        GridPane.setConstraints(mandatory, 4, j + rowIndex2);
+                        System.out.println(x.getOperation(newValue.intValue()- 1).getObjectFromResponse(i).getField(j).getName());
+                    }
+
+                    grid.getChildren().addAll(field, allowedValues, mandatory);
+                }
+            }
+
+
 
         });
 
+        // //add grid to scrollPane
+        scrollPane.setContent(grid);
 
         //declare scene
-        Scene scene = new Scene(grid, 1400, 700);
+        Scene scene = new Scene(scrollPane, 1400, 700);
 
         //show primary stage
         primaryStage.setScene(scene);
